8a70daaf8ec424bc74df904c3a653e045f5eb4bf
matto <m2harvey@uwaterloo.ca>
2015-06-02 15:28:16 -0700

    Getting rid of garbage in test.c
    This was my attempt at test driven development, but it turned out
    that I didn't know what I wanted, so now I'm just removing it.

--- test/test.c
+++ test/test.c
@@ -28,34 +28,5 @@ int main(int argc, char** argv)
             file.aspectratio_num, file.aspectratio_den,
             file.colourspace,
             file.comment);
-    y4mNextFrame(&file);
-    /*char** buffer = malloc(sizeof(char*) * file.width);
-    for (int j = 0; j < file.width; ++j)
-    {
-        buffer[j] = malloc(sizeof(char) * file.height * BYTES_PER_PIXEL);
-    }
-    for (int i = 0; i < file.frames; ++i)
-    {
-        //char** buffer = malloc(sizeof(char) * file.height * file.width * BYTES_PER_PIXEL);
-        for (int y = 0; y < file.height; ++y)
-        {
-            for (int x = 0; x < file.width; ++x)
-            {
-                //should this really be an int?
-                buffer[x][y*BYTES_PER_PIXEL + 0] = y4mGetRed(&file, x, y);
-                buffer[x][y*BYTES_PER_PIXEL + 1] = y4mGetGreen(&file, x, y);
-                buffer[x][y*BYTES_PER_PIXEL + 2] = y4mGetBlue(&file, x, y);
-                printf("%d %d %d\n", buffer[x][y*BYTES_PER_PIXEL + 0], buffer[x][y*BYTES_PER_PIXEL + 1], buffer[x][y*BYTES_PER_PIXEL + 2]);
-            }
-        }
-        assert (0 == y4mNextFrame(&file));
-
-    }
-    for (int j = 0; j < file.width; ++j)
-    {
-        free(buffer[j]);
-    }
-    free(buffer);
-    */
     y4mCloseFile(&file);
 }
