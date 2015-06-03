60b4568bd0ef767ba4d7c74b79ade40c9407400c
matto <m2harvey@uwaterloo.ca>
2015-06-02 22:39:08 -0700

    Adding a test with writing of file

--- test/test.c
+++ test/test.c
@@ -6,6 +6,33 @@
 
 #define BYTES_PER_PIXEL (3)
 
+enum operations
+{
+    OPERATION_DARKEN,
+    OPERATION_LIGHTEN,
+    OPERATION_INVERT
+};
+
+void change_data(char* frame_data, y4mFile_t* y4mfile, int operation)
+{
+    int yplane_bytes = y4mfile->width * y4mfile->height;
+    for (int i = 0; i < yplane_bytes; ++i)
+    {
+        switch (operation)
+        {
+        case OPERATION_DARKEN:
+            frame_data[i] -= 10;
+            break;
+        case OPERATION_LIGHTEN:
+            frame_data[i] += 10;
+            break;
+        case OPERATION_INVERT:
+            frame_data[i] = 255 - frame_data[i];
+            break;
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
     assert(argc == 2);
@@ -28,9 +55,17 @@ int main(int argc, char** argv)
             file.aspectratio_num, file.aspectratio_den,
             file.colourspace,
             file.comment);
+    //int op = 0;
     while(file.eof == 0)
     {
-        y4mGetFrameDataPointer(&file);
+        char* data_ptr = y4mGetFrameDataPointer(&file);
+        /*if (op)
+            change_data(data_ptr, &file, OPERATION_DARKEN);
+        else
+            change_data(data_ptr, &file, OPERATION_LIGHTEN);
+        op = !op;
+        */
+        change_data(data_ptr, &file, OPERATION_INVERT);
         y4mNextFrame(&file);
     }
     y4mWriteToFile(&file, "new.y4m");
